14c5a8f90b12e31dabb4872586017153b95efd31
FredrikAV <fredrik.viksten@liu.se>
2023-01-16 10:29:13 +0100

    Add support for downloading artefacts file

--- code/download_all_weed_data.py
+++ code/download_all_weed_data.py
@@ -11,9 +11,10 @@ def progressBar(current, total, width=80):
   sys.stdout.write("\r" + progress_message)
   sys.stdout.flush()
 
-def main(weed_file, root_folder, base_url):
+def main(weed_file, root_folder, base_url, remote_path):
     root_folder = root_folder + '/fielddata'
-    os.makedirs(root_folder + '/tractor-33-zipped', exist_ok = True)
+    os.makedirs(root_folder + remote_path, exist_ok = True)
+    base_url = base_url + remote_path
 
     download_list = None
     with open(weed_file) as downloads_file:
@@ -22,14 +23,14 @@ def main(weed_file, root_folder, base_url):
     for file in download_list:
         file_fixed = file.split('\n')[0]
         print("")
-        if (path.exists(root_folder + '/tractor-33-zipped/' + file_fixed)):
+        if (path.exists(root_folder + remote_path + file_fixed)):
             print('Skipping ' + base_url + '/' + file_fixed)
         else:
             print('Downloading ' + base_url + '/' + file_fixed)
-            wget.download(base_url + '/' + file_fixed, out=root_folder + '/tractor-33-zipped/', bar=progressBar)
+            wget.download(base_url + '/' + file_fixed, out=root_folder + remote_path, bar=progressBar)
 
     #extract each folder
-    tar_files = glob.glob(root_folder + '/tractor-33-zipped' + '/**/*.tar.gz', recursive=True)
+    tar_files = glob.glob(root_folder + remote_path + '/**/*.tar.gz', recursive=True)
     for file in tar_files:
         f = tarfile.open(name=file, mode='r:gz')
         print("")
@@ -38,13 +39,12 @@ def main(weed_file, root_folder, base_url):
 
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='download and extaction script for weed data')
     parser.add_argument('-f', '--file', type=str, help='file with list of weed data in tar.gz format', required=True)
     parser.add_argument('-r', '--root_download_folder', type=str, help='download root folder', required=True)
-    parser.add_argument('-b', '--base_url', type=str, default='https://openweeds.linkoping-ri.se/data/tractor-33-zipped', help='base url for tar files', required=False)    
+    parser.add_argument('-b', '--base_url', type=str, default='https://openweeds.linkoping-ri.se/data', help='base url for tar files', required=False)
+    parser.add_argument('-p', '--remote_path', type=str, default='/tractor-33-zipped', help='download artefacts instead of images', required=False)
 
     args = parser.parse_args()
-
-    main(weed_file=args.file, root_folder=args.root_download_folder, base_url=args.base_url)
+    main(weed_file=args.file, root_folder=args.root_download_folder, base_url=args.base_url, remote_path=args.remote_path)
